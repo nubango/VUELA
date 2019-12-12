9dac34a4e31a2bc29d627153b1ec44825fd8400e
Nuria <nubango@ucm.es>
2019-12-12 15:10:36 +0100

    Nuevo comienzo

---
+++ Vuela/Assets/uAdventureUnity/Editor/Writer/UnitySceneDOMWriter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using uAdventure.Editor;
+
+namespace uAdventure.Unity
+{
+    using CIP = ChapterDOMWriter.ChapterTargetIDParam;
+
+    [DOMWriter(typeof(UnityScene))]
+    public class UnitySceneDOMWriter : ParametrizedDOMWriter
+    {
+        protected override void FillNode(XmlNode node, object target, params IDOMWriterParam[] options)
+        {
+            var unityScene = target as UnityScene;
+            var element = node as XmlElement;
+
+            element.SetAttribute("id", unityScene.Id);
+            element.SetAttribute("scene", unityScene.Scene);
+
+
+            if (options.Any(o => o is CIP && (o as CIP).TargetId.Equals(unityScene.getId())))
+            {
+                element.SetAttribute("start", "yes");
+            }
+            else
+            {
+                element.SetAttribute("start", "no");
+            }
+        }
+
+        protected override string GetElementNameFor(object target)
+        {
+            return "unity-scene";
+        }
+    }
+}
---
+++ Vuela/Assets/uAdventureUnity/Model/Parser/UnitySceneDOMParser.cs
@@ -0,0 +1,32 @@
+﻿using System.Xml;
+using uAdventure.Core;
+
+namespace uAdventure.Unity
+{
+    [DOMParser("unity-scene")]
+    public class UnitySceneDOMParser : IDOMParser
+    {
+        public object DOMParse(XmlElement element, params object[] parameters)
+        {
+            var chapter = parameters[0] as Chapter;
+
+            var id = element.GetAttribute("id");
+            var scene = element.GetAttribute("scene");
+            var unityScene = new UnityScene
+            {
+                Id = id,
+                Scene = scene
+            }; 
+
+            bool initialScene = ExString.EqualsDefault(element.GetAttribute("start"), "yes", false);
+            
+            if (initialScene)
+            {
+                chapter.setTargetId(id);
+            }
+
+            return unityScene;
+
+        }
+    }
+}
